5ced1d194b9ba9c734de87753c7f5f2688672a81
Clément <clementfiesta@gmail.com>
2025-02-19 19:57:00 +0100

    Add available event verification

--- src/modules/discord/constants/messages.ts
+++ src/modules/discord/constants/messages.ts
@@ -1,11 +1,8 @@
-type Messages = {
-  [key: string]: string | ((...args: any[]) => string);
-};
-
-export const MESSAGES: Messages = {
+export const MESSAGES = {
   ready: (username: string) => `${username} ready`,
-};
+} as const;
 
-export const ERROR_MESSAGES: Messages = {
+export const ERROR_MESSAGES = {
   'no-discord-bot-token': 'No discord bot token provided',
-};
+  'unknown-event': (eventName: string) => `Unknown event: ${eventName}`,
+} as const;
--- src/modules/discord/services/discord-events.service.ts
+++ src/modules/discord/services/discord-events.service.ts
@@ -1,15 +1,24 @@
+import EVENTS from '@discord/constants/events';
+import { ERROR_MESSAGES } from '@discord/constants/messages';
 import events from '@discord/events';
 import { DiscordClientService } from '@discord/services/discord-client.service';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class DiscordEventsService implements OnModuleInit {
+  private logger = new Logger(DiscordEventsService.name);
+
   constructor(private readonly discordClientService: DiscordClientService) {}
 
   onModuleInit() {
     const client = this.discordClientService.client;
 
     for (const event of Object.values(events)) {
+      if (!EVENTS.includes(event.name)) {
+        this.logger.error(ERROR_MESSAGES['unknown-event'](event.name));
+        continue;
+      }
+
       if (event.once)
         client.once(event.name, (...args: any[]) =>
           // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
